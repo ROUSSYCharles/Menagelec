ffe214a0dcfe6407e9937aa34b917c5b0ce36734
ROUSSYCharles <roussy.charles21@gmail.com>
2024-10-07 14:37:55 +0200

    Ajoutez des fichiers projet.

---
+++ Entities/AbstractEntity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Menagelec.Entities
+{
+    abstract class AbstractEntity
+    {
+        public void hydrate()
+        {
+        
+        }
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,10 @@
+namespace Menagelec
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
